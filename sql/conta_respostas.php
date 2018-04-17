f224f60dd089a6348286b549798251a19dea6b29
AndrehAguiar <andreaguiar.g@gmail.com>
2018-04-17 14:05:32 -0300

    Update conta_respostas.php

--- sql/conta_respostas.php
+++ sql/conta_respostas.php
@@ -14,29 +14,29 @@
 	   die("Connection failed: " . $TIG->connect_error);
 	} 	
 
-	$RsQtdResposta =  "SELECT COUNT(*) AS qtd_respostas FROM u793605722_tig5.resposta WHERE (resposta.fk_pergunta = '".$pergunta."')"; 
+	$RsQtdResposta =  "SELECT COUNT(*) AS qtd_respostas FROM nomeBancoDados.resposta WHERE (resposta.fk_pergunta = '".$pergunta."')"; 
 
 	$result = mysqli_query($TIG, $RsQtdResposta);
 	$row_RsQtdResposta = mysqli_fetch_assoc($result);
 	
-	$RsNivel = "SELECT SUM(avaliacao.avaliacao) AS cls_respostas FROM u793605722_tig5.avaliacao
-		LEFT JOIN (u793605722_tig5.resposta)
+	$RsNivel = "SELECT SUM(avaliacao.avaliacao) AS cls_respostas FROM nomeBancoDados.avaliacao
+		LEFT JOIN (nomeBancoDados.resposta)
 			ON (resposta.fk_pergunta = '".$pergunta."')
 			WHERE (avaliacao.fk_resposta = resposta.id AND resposta.fk_pergunta = '".$pergunta."')";	
 
 	$result = mysqli_query($TIG, $RsNivel);
 	$row_RsNivel = mysqli_fetch_assoc($result);
 	
-	$RsNivelResposta = "SELECT SUM(avaliacao.avaliacao) AS cls_respostas FROM u793605722_tig5.avaliacao
-		LEFT JOIN (u793605722_tig5.resposta)
+	$RsNivelResposta = "SELECT SUM(avaliacao.avaliacao) AS cls_respostas FROM nomeBancoDados.avaliacao
+		LEFT JOIN (nomeBancoDados.resposta)
 			ON (resposta.id = avaliacao.fk_resposta)
 			WHERE (resposta.id = '".$id_resposta."')";	
 
 	$result = mysqli_query($TIG, $RsNivelResposta);
 	$row_RsNivelResposta = mysqli_fetch_assoc($result);
 
-	$RsNivelUser = "SELECT SUM(avaliacao.avaliacao) AS cls_respostas FROM u793605722_tig5.avaliacao
-		LEFT JOIN (u793605722_tig5.resposta)
+	$RsNivelUser = "SELECT SUM(avaliacao.avaliacao) AS cls_respostas FROM nomeBancoDados.avaliacao
+		LEFT JOIN (nomeBancoDados.resposta)
 			ON (resposta.fk_pergunta = '".$pergunta."')
 			WHERE (avaliacao.fk_resposta = '".$id_resposta."' AND avaliacao.fk_usuario = '".$user."')";	
 
