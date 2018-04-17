cb0508a6fb10ce9f692693672c393928d1d3798f
AndrehAguiar <andreaguiar.g@gmail.com>
2018-04-17 14:02:48 -0300

    Update pag_categoria.php

--- sql/pag_categoria.php
+++ sql/pag_categoria.php
@@ -2,7 +2,7 @@
 	 /* Constantes de configuração */  
  define('QTDE_REGISTROS', 3);   
  define('RANGE_PAGINAS', 1);   
-   
+
  /* Recebe o número da página via parâmetro na URL */  
  $pagina_atual = (isset($_GET['page']) && is_numeric($_GET['page'])) ? $_GET['page'] : 1;   
    
@@ -12,14 +12,14 @@
  /* Instrução de consulta para paginação com MySQL */  
 	 /* Cria uma conexão PDO com MySQL */  
 	 $opcoes = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8');  
-	 $TIG = new PDO("mysql:host=localhost; dbname=u793605722_tig5;", "u793605722_gti5t", "LqDyNy:?I1Sgehv`sZ", $opcoes); 
+	 $TIG = new PDO("mysql:host=localhost; dbname=nomeBancoDados;", "userLocal", "passUserLocal", $opcoes); 
  
 	$RsCategPerguntas =  "SELECT * FROM u793605722_tig5.materia
 	
-						JOIN u793605722_tig5.pergunta
+						JOIN nomeBancoDados.pergunta
 							ON (pergunta.fk_materia = materia.id)
 							
-						LEFT JOIN u793605722_tig5.users
+						LEFT JOIN nomeBancoDados.users
 							ON (users.id_usuario = pergunta.fk_usuario)
 							
 						WHERE materia.id = '".$categoria."'
@@ -31,7 +31,7 @@
 	 $dados = $stm->fetchAll(PDO::FETCH_OBJ);   
  
  /* Conta quantos registos existem na tabela */  
- $sqlContador = "SELECT COUNT(*) AS total_registros FROM u793605722_tig5.pergunta WHERE (pergunta.id = '".$categoria."')";   
+ $sqlContador = "SELECT COUNT(*) AS total_registros FROM nomeBancoDados.pergunta WHERE (pergunta.id = '".$categoria."')";   
  $conta = $TIG->prepare($sqlContador);   
  $conta->execute();   
  $valor = $conta->fetch(PDO::FETCH_OBJ);  
