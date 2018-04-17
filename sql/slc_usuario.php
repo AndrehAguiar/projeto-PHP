8548d2e1a1b91af10ec40245ce48e1bf2633dc9f
AndrehAguiar <andreaguiar.g@gmail.com>
2018-04-17 14:16:55 -0300

    Update slc_usuario.php

--- sql/slc_usuario.php
+++ sql/slc_usuario.php
@@ -2,9 +2,9 @@
 	// include("Connections/tig.php");
 
 	$hostname_TIG = "localhost";
-	$database_TIG = "u793605722_tig5";
-	$username_TIG = "u793605722_gti5t";
-	$password_TIG = "LqDyNy:?I1Sgehv`sZ";
+	$database_TIG = "nomeBancoDados";
+	$username_TIG = "userLocal";
+	$password_TIG = "senhaUserLocal";
 	$TIG = new mysqli( $hostname_TIG, $username_TIG, $password_TIG); 
 	mysqli_set_charset( $TIG, 'utf8' );
 
@@ -17,21 +17,21 @@
 	   die("Connection failed: " . $TIG->connect_error);
 	} 
 
-	$sl_user = mysqli_query( $TIG, "SELECT * FROM u793605722_tig5.users
+	$sl_user = mysqli_query( $TIG, "SELECT * FROM nomeBancoDados.users
 		WHERE users.id_usuario = '".$user."'" ) or die( mysqli_error( $TIG ) );
 
 
 	$slUser = mysqli_query($TIG, "SELECT * FROM u793605722_tig5.users
-									LEFT JOIN u793605722_tig5.interesse
+									LEFT JOIN nomeBancoDados.interesse
 										ON (users.email = interesse.email_usuario)
-									LEFT JOIN u793605722_tig5.cadastro
+									LEFT JOIN nomeBancoDados.cadastro
 										ON(cadastro.fk_usuario = users.id_usuario)
 									WHERE '".$user."' = cadastro.fk_usuario") or die( mysqli_error( $TIG ) );
 
 	$slInteresse = mysqli_query($TIG, "SELECT * FROM u793605722_tig5.materia
-									JOIN u793605722_tig5.interesse
+									JOIN nomeBancoDados.interesse
 										ON (interesse.fk_materia = materia.id)
-									 LEFT JOIN u793605722_tig5.users
+									 LEFT JOIN nomeBancoDados.users
 										ON (users.email = interesse.email_usuario)
 									WHERE users.id_usuario = '".$user."' ORDER BY interesse.id") or die( mysqli_error( $TIG ) );
 
