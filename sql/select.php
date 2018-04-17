4109673529329f4935a90d61c7a39c9ef40c8304
AndrehAguiar <andreaguiar.g@gmail.com>
2018-04-17 14:13:31 -0300

    Update select.php

--- sql/select.php
+++ sql/select.php
@@ -3,9 +3,9 @@
 	// include("Connections/tig.php");
 
 	$hostname_TIG = "localhost";
-	$database_TIG = "u793605722_tig5";
-	$username_TIG = "u793605722_gti5t";
-	$password_TIG = "LqDyNy:?I1Sgehv`sZ";
+	$database_TIG = "nomeBancoDados";
+	$username_TIG = "userLocal";
+	$password_TIG = "senhaUserLocal";
 	$TIG = new mysqli( $hostname_TIG, $username_TIG, $password_TIG); 
 	mysqli_set_charset( $TIG, 'utf8' );
 
@@ -17,22 +17,22 @@
 		$area = isset($_GET['area']);
 	}
 
-	$sl_categ = mysqli_query( $TIG, "SELECT * FROM u793605722_tig5.categoria" )
+	$sl_categ = mysqli_query( $TIG, "SELECT * FROM nomeBancoDados.categoria" )
 		or die( mysqli_error( $TIG ) ); 
 
-	$sl_materias = mysqli_query( $TIG, "SELECT * FROM u793605722_tig5.materia" )
+	$sl_materias = mysqli_query( $TIG, "SELECT * FROM nomeBancoDados.materia" )
 		or die( mysqli_error( $TIG ) );
 
-	$sl_mater = mysqli_query( $TIG, "SELECT * FROM u793605722_tig5.materia
+	$sl_mater = mysqli_query( $TIG, "SELECT * FROM nomeBancoDados.materia
 		WHERE materia.fk_categoria = '".$area."'" )
 		or die( mysqli_error( $TIG ) );
 
-	$sl_pergunta = mysqli_query( $TIG, "SELECT * FROM u793605722_tig5.pergunta
-											LEFT JOIN u793605722_tig5.resposta
+	$sl_pergunta = mysqli_query( $TIG, "SELECT * FROM nomeBancoDados.pergunta
+											LEFT JOIN nomeBancoDados.resposta
 											ON (resposta.fk_pergunta = pergunta.id)" )
 		or die( mysqli_error( $TIG ) ); 
 	
 
-	$RsCategorias = mysqli_query( $TIG, "SELECT * FROM u793605722_tig5.categoria" )or die( mysqli_error( $TIG ) );
+	$RsCategorias = mysqli_query( $TIG, "SELECT * FROM nomeBancoDados.categoria" )or die( mysqli_error( $TIG ) );
 
 ?>
