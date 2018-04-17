9bb73c6832a8735842ad0021ae7e4d74592e44e1
AndrehAguiar <andreaguiar.g@gmail.com>
2018-04-17 14:19:26 -0300

    Update update.php

--- sql/update.php
+++ sql/update.php
@@ -1,9 +1,9 @@
 ﻿<?php
 
 	$hostname_TIG = "localhost";
-	$database_TIG = "u793605722_tig5";
-	$username_TIG = "u793605722_gti5t";
-	$password_TIG = "LqDyNy:?I1Sgehv`sZ";
+	$database_TIG = "nomeBancoDados";
+	$username_TIG = "userLocal";
+	$password_TIG = "senhaUserLocal";
 
 	// Create connection
 	$TIG = new mysqli( $hostname_TIG, $username_TIG, $password_TIG); 
@@ -17,7 +17,7 @@
 	if(isset($_POST["edit_user"])){
 
 		$senha = sha1(md5($_POST["password"]));
-		$ed_user = "UPDATE u793605722_tig5.users SET  email='".$_POST["email"]."', formacao='".$_POST["formacao"]."', name='".$_POST["name"]."', sobrenome='".$_POST["sobrenome"]."', password='".$senha."' WHERE (users.id_usuario = '".$_POST["user"]."')";
+		$ed_user = "UPDATE nomeBancoDados.users SET  email='".$_POST["email"]."', formacao='".$_POST["formacao"]."', name='".$_POST["name"]."', sobrenome='".$_POST["sobrenome"]."', password='".$senha."' WHERE (users.id_usuario = '".$_POST["user"]."')";
 
 		if (mysqli_query($TIG, $ed_user)) {
 				header(sprintf('location: %s&user=atualizado', $_SERVER['HTTP_REFERER']));
@@ -29,7 +29,7 @@
 	 } 
 
 	if(isset($_POST["form_ed_perfil"])){
-		$ed_perfil = "UPDATE u793605722_tig5.cadastro SET telefone='".$_POST["celular"]."', endereco='".$_POST["endereco"]."', numero='".$_POST["numero"]."',  complemento='".$_POST["complemento"]."', bairro='".$_POST["bairro"]."', cidade='".$_POST["cidade"]."', estado='".$_POST["estado"]."', pais='".$_POST["pais"]."',  cep='".$_POST["cep"]."' WHERE (cadastro.fk_usuario = '".$_POST["usuario"]."')";
+		$ed_perfil = "UPDATE nomeBancoDados.cadastro SET telefone='".$_POST["celular"]."', endereco='".$_POST["endereco"]."', numero='".$_POST["numero"]."',  complemento='".$_POST["complemento"]."', bairro='".$_POST["bairro"]."', cidade='".$_POST["cidade"]."', estado='".$_POST["estado"]."', pais='".$_POST["pais"]."',  cep='".$_POST["cep"]."' WHERE (cadastro.fk_usuario = '".$_POST["usuario"]."')";
 
 		if (mysqli_query($TIG, $ed_perfil)) {
 				header(sprintf('location: %s&dados=atualizado', $_SERVER['HTTP_REFERER']));
