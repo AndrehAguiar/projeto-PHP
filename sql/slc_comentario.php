4e599ea28e62324dd9082ad28d315ec554e0fc9b
AndrehAguiar <andreaguiar.g@gmail.com>
2018-04-17 14:14:30 -0300

    Update slc_comentario.php

--- sql/slc_comentario.php
+++ sql/slc_comentario.php
@@ -2,12 +2,12 @@
 	//include("Connections/tig.php");
 
 	$hostname_TIG = "localhost";
-	$database_TIG = "u793605722_tig5";
-	$username_TIG = "u793605722_gti5t";
-	$password_TIG = "LqDyNy:?I1Sgehv`sZ";
+	$database_TIG = "nomeBancoDados";
+	$username_TIG = "userLocal";
+	$password_TIG = "senhaUserLocal";
 	$TIG = new mysqli( $hostname_TIG, $username_TIG, $password_TIG); 
 	mysqli_set_charset( $TIG, 'utf8' );	
-				$RsComenta = mysqli_query( $TIG, "SELECT * FROM u793605722_tig5.comentario
+				$RsComenta = mysqli_query( $TIG, "SELECT * FROM nomeBancoDados.comentario
 				WHERE comentario.fk_resposta = '".$id_resposta."'" )or die( mysqli_error( $TIG ) );
 
 	$TIG->close();
