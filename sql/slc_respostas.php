2073972d06af33150fe10bf682a49e48ad8b0146
AndrehAguiar <andreaguiar.g@gmail.com>
2018-04-17 14:15:33 -0300

    Update slc_respostas.php

--- sql/slc_respostas.php
+++ sql/slc_respostas.php
@@ -8,8 +8,8 @@
 	if ($TIG->connect_error) {
 	   die("Connection failed: " . $TIG->connect_error);
 	}
-	$RsRespostas = mysqli_query( $TIG, "SELECT * FROM u793605722_tig5.users
-										LEFT JOIN u793605722_tig5.resposta
+	$RsRespostas = mysqli_query( $TIG, "SELECT * FROM nomeBancoDados.users
+										LEFT JOIN nomeBancoDados.resposta
 											ON (users.id_usuario = resposta.fk_usuario)
 										WHERE resposta.fk_pergunta = '".$pergunta."'
 										 ORDER BY resposta.id DESC" )or die( mysqli_error( $TIG ) );
